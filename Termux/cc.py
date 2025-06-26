7d485284319864ba7af5aa6e73289b57ed81efe9
King492-cmd <mahboobhussain8795@gmail.com>
2025-06-26 10:51:20 +0530

    Update cc.py
    Agar app ko is tra chat bot bana na hai to telegram pe jao aur likho fathe bot aur fir new bot par click karo aur app ko ek pass key fila ga jis ko Token key bolte hai code me token key likha hoo ga waha app apne key ko daal do chat bot tumra ho jaye ga 
    
    Signed-off-by: King492-cmd <mahboobhussain8795@gmail.com>

--- Termux/cc.py
+++ Termux/cc.py
@@ -1,1 +1,41 @@
-king
+import logging
+import threading
+from telegram import (
+    Update,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    Poll,
+)
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    CallbackQueryHandler,
+    PollAnswerHandler,
+    CallbackContext,
+)
+
+# --- Logging Setup ---
+logging.basicConfig(format='%(asctime)s - %(name)s - %(lev>
+
+# --- Bot Token ---
+# --- TOKEN = "7037066343:AAGUozQAv3PGePUYPtsVMdhDvG6eYaoplmM" ---
+
+# --- Sample Class 12 Questions Structure ---
+QUESTIONS = {
+    "excel question": {
+          "Basic": [
+    {
+        "question": "1. Excel में Array Formula क्या होता है?",
+        "options": [
+            "(A) एक फॉर्मूला जो कई सेल्स पर एक साथ काम करता है",
+            "(B) केवल एक सेल के लिए फॉर्मूला",
+            "(C) डेटा जोड़ने का तरीका",
+            "(D) केवल टेक्स्ट के लिए फॉर्मूला"
+        ],
+        "correct_option": 0
+    },
+
+
+    # --- Add Questions 9999+ add kar sakte ho ---
+
+            
